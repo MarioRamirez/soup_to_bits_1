76c473c1a26c2adf0a903bcbae2f05314968c632
Klez <guildonlypvp@gmail.com>
2014-11-23 23:38:18 +0000

    Añadidas las acciones new, edit, update y destroy al controlador de Soups

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -7,4 +7,21 @@ class SoupsController < ApplicationController
       format.json {render json: @soup}
     end
   end
+  
+  def index
+    @soups = Soup.all
+    
+    respond_to do |format|
+      format.json {render json: @soups}
+    end
+  end
+  
+  def new
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
 end
